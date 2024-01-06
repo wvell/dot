e282b8b7e968bae1831f75633116fde51ef242e8
Wytse Vellema <w.vellema@slash2.nl>
2024-01-06 20:44:47 +0100

    Add new handy keymaps

--- nvim/lua/wvell/keymap.lua
+++ nvim/lua/wvell/keymap.lua
@@ -2,31 +2,39 @@ local wk = require("which-key")
 
 -- window management
 wk.register({
-  w = {
-    name = "Windows",
-    v = { "<C-w>v", "Split window vertically" },
-    h = { "<C-w>s", "Split window horizontally" },
-    e = { "<C-w>=", "Make splits equal size" },
-    x = { "<cmd>close<CR>", "Close current split" },
-  },
+	w = {
+		name = "Windows",
+		v = { "<C-w>v", "Split window vertically" },
+		h = { "<C-w>s", "Split window horizontally" },
+		e = { "<C-w>=", "Make splits equal size" },
+		x = { "<cmd>close<CR>", "Close current split" },
+	},
 }, { prefix = "<leader>" })
 
 -- Tab management
 wk.register({
-  t = {
-    name = "Tabs",
-    o = { "<cmd>tabnew<CR>", "Open new tab" },
-    x = { "<cmd>tabclose<CR>", "Close current tab" },
-    n = { "<cmd>tabn<CR>", "Go to next tab" },
-    p = { "<cmd>tabp<CR>", "Go to previous tab" },
-    f = { "<cmd>tabnew %<CR>", "Open current buffer in new tab" },
-  },
+	t = {
+		name = "Tabs",
+		o = { "<cmd>tabnew<CR>", "Open new tab" },
+		x = { "<cmd>tabclose<CR>", "Close current tab" },
+		n = { "<cmd>tabn<CR>", "Go to next tab" },
+		p = { "<cmd>tabp<CR>", "Go to previous tab" },
+		f = { "<cmd>tabnew %<CR>", "Open current buffer in new tab" },
+	},
 }, { prefix = "<leader>" })
 
 -- buffer management
 wk.register({
-  b = {
-    name = "Buffers",
-    d = { "<cmd>bd<CR>", "Delete current buffer" },
-  },
+	b = {
+		name = "Buffers",
+		d = { "<cmd>bd<CR>", "Delete current buffer" },
+	},
 }, { prefix = "<leader>" })
+
+-- Move lines up and down in visual mode.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Center the screen when navigating with C-d and C-u.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
