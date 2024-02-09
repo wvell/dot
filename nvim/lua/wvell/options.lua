d8255a0014ce21c8c6b4a8c0165797fb3941e395
Wytse Vellema <w.vellema@slash2.nl>
2024-02-09 14:34:11 +0100

    Do not use system clipboard for nvim

--- nvim/lua/wvell/options.lua
+++ nvim/lua/wvell/options.lua
@@ -27,7 +27,7 @@ opt.background = "dark" -- colorschemes that can be light or dark will be made d
 opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
